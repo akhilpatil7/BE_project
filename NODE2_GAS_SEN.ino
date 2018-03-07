507d867ab450d63021a776ce786bbdeb29d4eae8
akhilpatil7 <akhil.patil7@gmail.com>
2018-03-07 22:36:15 +0530

    implemented Blynk Bridge

--- NODE2_GAS_SEN.ino
+++ NODE2_GAS_SEN.ino
@@ -1,23 +1,36 @@
 #include <BlynkSimpleEsp8266.h>
-#include <FirebaseArduino.h>
+   #include <FirebaseArduino.h>
 #include  <ESP8266WiFi.h>
 #define FIREBASE_HOST "aquaponics-7796.firebaseio.com"
 #define FIREBASE_AUTH "b8IeSWlnOeg1XiUjCxE0gpWMP3uRcnyLosyXDRzH"
 #define BLYNK_PRINT Serial
 char ssid[] = "TP-LINK_3880";
 char pass[] = "sid123456";
-char auth[] = "3f72f769a9f34eaaa733d3df5d3dcfb4";
+//char auth[] = "3f72f769a9f34eaaa733d3df5d3dcfb4";
+char auth[] = "120e54b4b0de435385b4438f30879f9a";
 
+WidgetBridge bridge1(V1);
+BlynkTimer timer;
 int input = A0;
 int output = 7;
 int th=500; // Set threshold level.
 String airqual;
 
+BLYNK_CONNECTED() {
+  bridge1.setAuthToken("3f72f769a9f34eaaa733d3df5d3dcfb4"); // Place the AuthToken of the second hardware here
+}
+
 //BLYNK_READ(V1) {
 //  Blynk.virtualWrite(V1, airqual);
 //}
 
-WidgetLCD lcd(V1);
+void blynkAnotherDevice()
+{
+    bridge1.virtualWrite(V1,airqual);
+}
+
+
+//WidgetLCD lcd(V1);
 
 void setup()
 {
@@ -26,7 +39,11 @@ Serial.begin(9600);
 //digitalWrite(output,LOW);
 Blynk.begin(auth,ssid,pass);
 Firebase.begin(FIREBASE_HOST,FIREBASE_AUTH);
+timer.setInterval(1000L, blynkAnotherDevice);
 }
+
+
+
 void loop()
 {
 Serial.println(analogRead(input));
@@ -36,8 +53,8 @@ if(analogRead(input)>th)
 Serial.println("HIGH");
 airqual = "HIGH";
 Firebase.setString("Air","HIGH");
- lcd.clear(); //Use it to clear the LCD Widget
-  lcd.print(4, 0, "HIGH"); // use: (position X: 0-15, position Y: 0-1, "Message you want to print")
+ //lcd.clear(); //Use it to clear the LCD Widget
+ // lcd.print(4, 0, "HIGH"); // use: (position X: 0-15, position Y: 0-1, "Message you want to print")
  // lcd.print(4, 1, "World");
 }
 else
@@ -46,10 +63,12 @@ else
 Serial.println("LOW");
 airqual = "LOW";
 Firebase.setString("Air","LOW");
-lcd.clear(); //Use it to clear the LCD Widget
-  lcd.print(4, 0, "LOW"); // use: (position X: 0-15, position Y: 0-1, "Message you want to print")
+//lcd.clear(); //Use it to clear the LCD Widget
+  //lcd.print(4, 0, "LOW"); // use: (position X: 0-15, position Y: 0-1, "Message you want to print")
  
 }
 delay(500);
  Blynk.run();
+ timer.run();
+
 }
